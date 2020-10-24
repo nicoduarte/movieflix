b3db280c8cfb07d1113e7ea468ce7d3fe289a8f4
nicolasduarte <nicolas.duarte@infinixsoft.com>
2020-10-24 20:31:00 -0300

    Add SearchActivity.kt

---
+++ app/src/main/java/com/nicoduarte/movieflix/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.nicoduarte.movieflix.ui.search
+
+import android.os.Bundle
+import com.nicoduarte.movieflix.R
+import com.nicoduarte.movieflix.ui.BaseActivity
+
+class SearchActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
